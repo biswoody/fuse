d02ac714cb6fa70741fe750601c567621b0309aa
biswoody <geeantwi455@gmail.com>
2024-07-02 00:24:38 -0500

    Add files via upload

---
+++ imak.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.56.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_iam_user" "code" {
+  name = "nat"
+}
